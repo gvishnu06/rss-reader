3e40c73b71486b16b67e0d6cbce1f705514c5800
Vishnu <vishnu.gondlekar@in.tesco.com>
2015-10-08 19:22:02 +0530

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var router = express.Router();
+var feed = require("feed-read");
+var request = require('request');
+
+/* GET home page. */
+router.get('/news', function(req, res, next) {
+  feed("http://craphound.com/?feed=rss2", function(err, articles) {
+  	if (err)
+  	{
+  		res.status(400).end();
+  	}
+  	if(articles)
+  	{
+  		if(articles.length == 0)
+  		{
+  			res.status(204).end();
+  		}
+  		else
+  		{
+  			res.send({"news":articles});
+  		}
+  	}
+  });
+});
+
+router.post('/search',function(req,res,next){
+	var searchUrl = req.body.searchUrl;
+	if(searchUrl)
+	{
+		feed("http://craphound.com/?feed=rss2", function(err, articles) {
+  			if (err)
+  			{
+  				res.status(400).end();
+  			}
+  			if(articles)
+	  		{
+  				if(articles.length == 0)
+  				{
+  					res.status(204).end();
+  				}
+  				else
+  				{
+  					res.send({"news":articles});
+  				}
+  			}
+  		});
+	}
+});
+
+module.exports = router;
